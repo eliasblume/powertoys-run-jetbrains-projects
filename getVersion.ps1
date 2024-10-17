24b805563c7588ca4e3a36412f47a9b50ebb15c4
elias <mail@elias-blu.me>
2024-10-17 18:34:16 +0200

    feat(actions): update actions

---
+++ getVersion.ps1
@@ -0,0 +1,6 @@
+﻿$jsonContent = Get-Content -Path "Community.PowerToys.Run.Plugin.JetbrainsProjects/plugin.json" -Raw
+
+$jsonObject = $jsonContent | ConvertFrom-Json
+
+$version = $jsonObject.Version
+
